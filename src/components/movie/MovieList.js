53765ff6a6095628cae91a302da21a7cd120097a
LeeJaeLee <cuonggaayf01@gmail.com>
2022-07-14 13:59:22 +0700

    MovieList

---
+++ src/components/movie/MovieList.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { SwiperSlide, Swiper } from "swiper/react";
+import MovieCard from "./MovieCard";
+import useSWR from "swr";
+import { fetcher } from "../../config";
+
+//https://api.themoviedb.org/3/movie/now_playing?api_key=<<api_key>>
+
+const MovieList = ({ type }) => {
+  const [movies, setMovies] = useState([]);
+  const { data, error } = useSWR(
+    `https://api.themoviedb.org/3/movie/${type}?api_key=4037a7bba03884bf5a7ccd8b780bd269`,
+    fetcher
+  );
+  //   console.log(data);
+  useEffect(() => {
+    if (data && data.results) {
+      setMovies(data.results);
+    }
+  }, [data]);
+  //   console.log(movies);
+  return (
+    <div className="movie-list">
+      <Swiper grabCursor={"true"} spaceBetween={40} slidesPerView={"auto"}>
+        {movies.length > 0 &&
+          movies.map((item) => (
+            <SwiperSlide key={item.div}>
+              <MovieCard item={item}></MovieCard>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default MovieList;
